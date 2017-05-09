a33c4079857599c3a628aec3b9cea842fdbd6557
KAZE\harunakaze <haruna.kaze@gmail.com>
2017-05-09 16:18:24 +0700

    + Tutorial Text

--- controller/KategoriUtamaController.php
+++ controller/KategoriUtamaController.php
@@ -1,7 +1,10 @@
 <?php
 class KategoriUtamaController extends Controller {
     public function index() {
+        // Pake model dengan fungsi loadModel, parameternya nama file di folder model, tanpa tambahan kata Model
         $model = $this->loadModel('kategoriUtama');
+
+        // Akses seperti object biasa
         $data = $model->selectKategoriWithKode("K22");
 
         $this->data['kodeKategori'] = $data['kode'];
